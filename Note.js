b080c82eab4bfdaa9ee7b75b0ea46d222606f34e
Daniel <devdonny9@gmail.com>
2019-03-20 15:12:08 +0100

    Added note from freq functionality in Note class

--- Note.js
+++ Note.js
@@ -1,8 +1,9 @@
 const frequency = require('./util/frequency');
 const Comparison = require('./Comparison');
+const Error = require('./Error');
 
 class Note {
-    constructor (name, octave, rootNote) {
+    constructor (name, octave, freq, rootNote) {
         this.name = name;
         this.octave = octave;
         this.tuning = {
@@ -10,7 +11,20 @@ class Note {
             octave: rootNote.octave,
             frequency: rootNote.frequency
         };
-        this.frequency = frequency.fromNote(this, this.tuning);
+        if (freq === null) {
+            this.frequency = frequency.fromNote(this, this.tuning);
+            this.centsOff = 0;
+        } else {
+            this.frequency = freq;
+            let checkedName = name || null;
+            let checkedOctave = octave || null;
+            let closest = frequency.getCentsOffFromNote(checkedName, checkedOctave, freq, rootNote);
+            if (closest !== false) {
+                this.centsOff = closest.cents;
+                this.name = closest.note.name;
+                this.octave = closest.note.octave;
+            } else this.err = new Error(7, `The frequency "${freq}" is out of range`, freq);
+        }
     }
 
     transpose (semitones) {
