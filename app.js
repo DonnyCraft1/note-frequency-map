e060aeabc00031c9f9e026e1d212cc4c955fefa1
Daniel <devdonny9@gmail.com>
2019-03-22 22:37:50 +0100

    fromFreq() and fromName() array support

--- app.js
+++ app.js
@@ -26,8 +26,36 @@ function noteFromFreq (freq) {
     return new Note(null, null, freq, rootNote);
 }
 
+function noteFromNameExport (notes) {
+    // If it's an array
+    if (Array.isArray(notes)) {
+        let result = [];
+        for (let i = 0; i < notes.length; i++) {
+            result.push(noteFromName(notes[i]));
+        }
+        return result;
+    // If it's not an array
+    } else {
+        return noteFromName(notes);
+    }
+}
+
+function noteFromFreqExport (freqs) {
+    // If it's an array
+    if (Array.isArray(freqs)) {
+        let result = [];
+        for (let i = 0; i < freqs.length; i++) {
+            result.push(noteFromFreq(freqs[i]));
+        }
+        return result;
+    // If it's not an array
+    } else {
+        return noteFromFreq(freqs);
+    }
+}
+
 module.exports = {
     setRoot: (string, freq) => setRoot(string, freq),
-    noteFromName: string => noteFromName(string),
-    noteFromFreq: float => noteFromFreq(float)
+    noteFromName: string => noteFromNameExport(string),
+    noteFromFreq: float => noteFromFreqExport(float)
 }
